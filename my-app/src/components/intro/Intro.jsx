a0fbf1233d774e6066bab813ca9f164ea2f0489d
audre <audre@DESKTOP-65H2D4B>
2022-02-17 21:35:29 -0600

    Worked on intro. Stopped at svg pasting. 23:12

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -1,8 +1,34 @@
+import "./intro.css"
+import meforwebsite from ".../.../.../img/meforwebsite.png"
+
+
 const Intro = () => {
     return (
-        <div>
+        <div className = "i">
+            <div className="i-left">
+                <div className = "i-left-wrapper">
+                    <h2 className = "i-intro">Hello, my name is </h2>
+                    <h2 className = "i-name">audrey</h2>
+                    <div className = "i-title">
+                        <div className = "i-title-wrapper">
+                            <div className = "i-title-item">Computer Science Student</div>
+                            <div className = "i-title-item">Skill 1</div>
+                            <div className = "i-title-item">Skill 2</div>
+                            <div className = "i-title-item">Skill 2</div>
+                        </div>
+                    </div>
+                    <p className="i-desc">
+                        I'm currently a  junior and am excited to graduate in December 2023
+                    </p>
+                </div>
 
-            this is intro
+                
+
+            </div>
+            <div className = "i-right">
+                <div className="i-bg"></div>
+                <image src = "{meforwebsite}" alt = "" className="i-img"></image>
+            </div>
         </div>
         )
 }
